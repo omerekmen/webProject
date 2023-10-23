368ec89d756e4f1c38d9ba3015b71ff8f8296b6d
Ömer <72146934+omerekmen@users.noreply.github.com>
2023-10-23 21:48:18 +0000

    DB

--- members/models.py
+++ members/models.py
@@ -1,86 +1,59 @@
 from django.db import models
 
-# Create your models here.
-"""
-Members
--
-memberID PK int
-userType ENUM
-identification VARCHAR(255) # TC No veya Pasaport No
-password VARCHAR(255) # Şifrelenmiş password
-firstName string
-lastName string
-phoneNumber int
-email VARCHHAR(255)
-birthDate Date 
-gender ENUM 
-weight Decimal(5,2)
-height Decimal(4,2)
-brandID int # Hiyerarşik olarak bağlı olunan şirket
-vendorID int # Finansal olarak bağlı olunan şirket
-campusID int # Bağlı olunan kampüs
-levelName ENUM # Education Lvl (Primary School, High School)
-classID int # Öğrencinin hangi sınıfa bağlı olduğu
-registrationDate Date 
-lastUpdated Ttimestamp
-isActive Boolean
-ipAdress VARCHAR(45)
-registrationState ENUM # New or returning student
-additionalData JSON
-Season Date # Season Info
+# Define the StudentLevels model
+class StudentLevels(models.Model):
+    LevelID = models.AutoField(primary_key=True)
+    LevelName = models.CharField(max_length=100)  # Adjust max_length as needed
+    CampusID = models.ForeignKey('Campus', on_delete=models.CASCADE)
 
-MemberAdress
---------------
-id PK int
-memberID int FK >- Members.memberID
-adressLine1 VARCHAR(256)
-adressLine2 VARCHAR(256)
-city VARCHAR
-country VARCHAR
-phoneNumber int
+# Define the CampusAddress model
+class CampusAddress(models.Model):
+    CampusAddressID = models.AutoField(primary_key=True)
+    FullAddress = models.CharField(max_length=255)
+    District = models.CharField(max_length=50)
+    City = models.CharField(max_length=50)
+    PostalCode = models.IntegerField()
+    AuthorizedFirstName = models.CharField(max_length=50)
+    AuthorizedLastName = models.CharField(max_length=50)
+    AuthorizedEmail = models.EmailField(max_length=100)
+    AuthorizedPhone = models.CharField(max_length=15)
 
-I want you to create these models with connections
-"""
+# Define the Campus model
+class Campus(models.Model):
+    CampusID = models.AutoField(primary_key=True)
+    CampusAddressID = models.ForeignKey(CampusAddress, on_delete=models.CASCADE)
+    FranchiseState = models.BooleanField()
+    Phone = models.CharField(max_length=255)
+    Email = models.EmailField(max_length=255)
 
+# Define the Class model
+class Class(models.Model):
+    ClassID = models.AutoField(primary_key=True)
+    ClassName = models.CharField(max_length=255)
+    ClassDescription = models.TextField()
+
+# Define the Members model
 class Members(models.Model):
-    memberID = models.AutoField(primary_key=True)
-    userType = models.CharField(max_length=20)
-    identification = models.CharField(max_length=255)
-    password = models.CharField(max_length=255)
-    firstName = models.CharField(max_length=255)
-    lastName = models.CharField(max_length=255)
-    phoneNumber = models.IntegerField()
-    email = models.CharField(max_length=255)
-    birthDate = models.DateTimeField()
-    gender = models.CharField(max_length=10)
-    weight = models.DecimalField(max_digits=5, decimal_places=2)
-    height = models.DecimalField(max_digits=4, decimal_places=2)
-    brandID = models.IntegerField()
-    vendorID = models.IntegerField()
-    campusID = models.IntegerField()
-    levelName = models.CharField(max_length=20)
-    classID = models.IntegerField()
-    registrationDate = models.DateTimeField()
-    lastUpdated = models.DateTimeField()
-    isActive = models.BooleanField()
-    ipAdress = models.CharField(max_length=45)
-    registrationState = models.CharField(max_length=20)
-    additionalData = models.JSONField()
-    Season = models.DateTimeField()
+    MemberID = models.AutoField(primary_key=True)
+    UserType = models.CharField(max_length=100)
+    Identification = models.CharField(max_length=255, unique=True)
+    PhoneNumber = models.CharField(max_length=20)
+    FirstName = models.CharField(max_length=255)
+    LastName = models.CharField(max_length=255)
+    BirthDate = models.DateField()
+    Email = models.EmailField(max_length=255, unique=True)
+    PasswordHash = models.CharField(max_length=255)
+    CampusID = models.ForeignKey(Campus, on_delete=models.CASCADE, null=True, blank=True)
+    Gender = models.CharField(max_length=100)
+    LevelID = models.ForeignKey(StudentLevels, on_delete=models.CASCADE, null=True, blank=True)
+    ClassID = models.ForeignKey(Class, on_delete=models.CASCADE, null=True, blank=True)
+    RegistrationState = models.CharField(max_length=100)
+    RegistrationDate = models.DateTimeField()
+    LastLogin = models.DateField()
+    LastUpdated = models.DateTimeField()
+    IsActive = models.BooleanField()
+    IPAddress = models.CharField(max_length=45)
+    Season = models.DateField()
 
     def __str__(self):
-        return self.firstName + " " + self.lastName
-    
-
-class MemberAddress(models.Model):
-    id = models.AutoField(primary_key=True)
-    memberID = models.ForeignKey(Members, on_delete=models.CASCADE)
-    adressLine1 = models.CharField(max_length=256)
-    adressLine2 = models.CharField(max_length=256)
-    city = models.CharField(max_length=256)
-    country = models.CharField(max_length=256)
-    phoneNumber = models.IntegerField()
-
-    def __str__(self):
-        return self.adressLine1 + " " + self.adressLine2
-        
+        return f"{self.FirstName} {self.LastName}"
--- products/models.py
+++ products/models.py
@@ -1,3 +1,79 @@
 from django.db import models
 
-# Create your models here.
+
+
+# Define the ProductCategory model
+class ProductCategory(models.Model):
+    ProductCategoryID = models.AutoField(primary_key=True)
+    CategoryName = models.CharField(max_length=255)  # You can adjust the max length as needed
+
+# Define the ProductCategorySizes model
+class ProductCategorySizes(models.Model):
+    CategorySizeID = models.AutoField(primary_key=True)
+    ProductCategoryID = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
+    ProductSize = models.CharField(max_length=100)
+
+# Define the ProductStock model
+class ProductStock(models.Model):
+    StockID = models.AutoField(primary_key=True)
+    RealStock = models.IntegerField()
+    SaleStock = models.IntegerField()
+    ReservedStock = models.IntegerField()
+    AcceptStock = models.IntegerField()
+    ReturnStock = models.IntegerField()
+    CancelStock = models.IntegerField()
+    PreproductionStock = models.IntegerField()
+
+# Define the ProductPrice model
+class ProductPrice(models.Model):
+    PriceID = models.AutoField(primary_key=True)
+    SalePrice = models.DecimalField(max_digits=10, decimal_places=2)
+    StrikedPrice = models.IntegerField()
+    DiscountRatio = models.IntegerField()
+    DiscountPrice = models.IntegerField()
+    DiscountType = models.CharField(max_length=100)
+    TaxPrice = models.CharField(max_length=100)
+    CombinePriceInfo = models.JSONField()
+
+# Define the CombinationProduct model
+class CombinationProduct(models.Model):
+    CProductID = models.AutoField(primary_key=True)
+    SProductInfo = models.JSONField()
+    CProductQuantity = models.IntegerField()
+
+# Define the SetProduct model
+class SetProduct(models.Model):
+    SetID = models.AutoField(primary_key=True)
+    SProductInfo = models.JSONField()
+    SProductQuantity = models.IntegerField()
+
+# Define the Products model
+class Products(models.Model):
+    ProductID = models.AutoField(primary_key=True)
+    ProductCategoryID = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
+    CategorySizeID = models.ForeignKey(ProductCategorySizes, on_delete=models.CASCADE)
+    product_type = models.CharField(max_length=100)
+    product_production_name = models.CharField(max_length=255)
+    product_name = models.CharField(max_length=255)
+    product_web_name = models.CharField(max_length=255)
+    product_production_date = models.DateField()
+    SKU_code = models.CharField(max_length=100)
+    barcode = models.CharField(max_length=100)
+    model_code = models.CharField(max_length=100)
+    StockID = models.ForeignKey(ProductStock, on_delete=models.CASCADE)
+    PriceID = models.ForeignKey(ProductPrice, on_delete=models.CASCADE)
+    product_color = models.CharField(max_length=100)
+    book_type = models.CharField(max_length=100)
+    product_state = models.IntegerField()
+    product_genre = models.CharField(max_length=100)
+    product_class = models.CharField(max_length=100)
+    product_level = models.CharField(max_length=100)
+    product_image = models.URLField()
+    product_measure_unit = models.CharField(max_length=100)
+    product_created_at = models.DateTimeField()
+    product_last_update = models.DateTimeField()
+    product_season = models.DateField()
+    product_change_limit = models.JSONField()
+
+    def __str__(self):
+        return f"{self.product_name}"
