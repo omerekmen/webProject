f66ab6d744f8833ea27f03c42bc2964ab849bbad
Ömer <72146934+omerekmen@users.noreply.github.com>
2023-11-03 10:45:29 +0000

    LATEST

---
+++ members/urls.py
@@ -0,0 +1,9 @@
+from django.contrib.auth.views import LoginView, LogoutView
+from django.urls import path
+from .views import * 
+
+urlpatterns = [
+    path("register/", register_view, name='register'),
+    path("login/", login_view, name='login'),
+    path("logout/", LogoutView.as_view(), name='logout'),
+]
--- members/views.py
+++ members/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from .forms import LoginForm, RegisterForm
+from django.contrib.auth import login, authenticate
+from django.contrib import messages
 
-# Create your views here.
+
+def register_view(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST or None)
+        if form.is_valid():
+            new_user = form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f"Merhaba {username}, hesabın oluşturuldu")
+            new_user = authenticate(username= form.cleaned_data['username'],
+                                    password= form.cleaned_data['password1'])
+            login(request, new_user)
+            return redirect("index")
+
+    else:
+        form = RegisterForm()
+
+    return render(request, 'registration/register.html', {'form': form})   
+
+def login_view(request):
+    if request.method == 'GET':
+        form = LoginForm()
+        return render(request, 'registration/login.html', {'form': form})   
