b4164ae6bcb999f8bbcb37d532147ce7c8b781c8
poseidonpp14 <103637928+poseidonpp14@users.noreply.github.com>
2023-12-19 09:50:08 +0000

    PREHOTFIX TO BE MERGED

--- schools/views.py
+++ schools/views.py
@@ -13,3 +13,12 @@ def get_page_content(request):
         except SchoolPages.DoesNotExist:
             return JsonResponse({'error': 'Page not found'}, status=404)
     return JsonResponse({'error': 'Invalid request'}, status=400)
+
+def fetch_content_for_modal(request):
+    page_url = request.GET.get('page_url')
+    try:
+        page = SchoolPages.objects.get(page_url=page_url)
+        content = page.page_content
+        return JsonResponse({'content': content})
+    except SchoolPages.DoesNotExist:
+        return JsonResponse({'error': 'Content not found'}, status=404)
--- webProject/urls.py
+++ webProject/urls.py
@@ -2,11 +2,14 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.conf import settings
+from schools.views import fetch_content_for_modal
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include('store.urls')),
     path('ckeditor/', include('ckeditor_uploader.urls')),
+    path('schools/', include('schools.urls')),  # Include your app's URLs
+    path('fetch-modal-content/', fetch_content_for_modal, name='fetch_modal_content')
 ]
 
 if settings.DEBUG == True:
