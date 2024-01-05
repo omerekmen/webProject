b40f7e109dc2629abd1f65a134a7eab1337db825
Ömer <72146934+omerekmen@users.noreply.github.com>
2024-01-05 16:08:06 +0000

    ...

--- webProject/urls.py
+++ webProject/urls.py
@@ -3,12 +3,12 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.conf import settings
 from schools.views import fetch_content_for_modal
-from wagtail.admin import urls as wagtailadmin_urls
+# from wagtail.admin import urls as wagtailadmin_urls
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include('store.urls')),
-    path('cms/', include(wagtailadmin_urls), name='wagtailadmin'),
+    # path('cms/', include(wagtailadmin_urls), name='wagtailadmin'),
     path('ckeditor/', include('ckeditor_uploader.urls')),
     path('schools/', include('schools.urls')),  # Include your app's URLs
     path('fetch-modal-content/', fetch_content_for_modal, name='fetch_modal_content')
